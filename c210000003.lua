60dac1e644cb96104e9015a94aa5e91ee2fb7df7
kasukajima <zetta_minamimoto@softbank.ne.jp>
2021-12-07 09:16:58 +0900

    コメント修正

--- c210000003.lua
+++ c210000003.lua
@@ -1,24 +1,24 @@
---ハニーボット
+--003
 function c210000003.initial_effect(c)
 	--link summon
-	--aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkRace,RACE_CYBERSE),2,2) --サイバース族2体以上2体以下
+	--aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkRace,RACE_CYBERSE),2,2) --特定の属性を持つオブジェクト2個以上2個以下
 	aux.AddLinkProcedure(c,c210000003.matfilter,1,1) --下記関数で定義する
 	c:EnableReviveLimit()
 	--cannot be target/battle indestructable
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CHANGE_CODE) --カード名を変更する
-	e1:SetRange(LOCATION_MZONE) --このカードが存在すべき場所
+	e1:SetCode(EFFECT_CHANGE_CODE) --オブジェクト名を変更する
+	e1:SetRange(LOCATION_MZONE) --発火時にこのオブジェクトが存在するべき場所
 	e1:SetTarget(c210000003.tgtg)
 	e1:SetValue(210000002)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
-	e2:SetType(EFFECT_TYPE_FIELD) --他のカードに影響する効果
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE) --効果を及ぼす対象がいるべき場所。複数なら複数個の引数
+	e2:SetType(EFFECT_TYPE_FIELD) --他のオブジェクトに影響する関数であることを示す
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE) --影響を及ぼすオブジェクトがいるべき場所。複数個の引数
 	c:RegisterEffect(e2)
 end
 function c210000003.matfilter(c)  
-    return c:IsSummonType(SUMMON_TYPE_NORMAL) and c:IsLinkSetCard(0x44D)  --通常召喚された「夜鋏」カテゴリ
+    return c:IsSummonType(SUMMON_TYPE_NORMAL) and c:IsLinkSetCard(0x44D)  --TYPE_NORMALを持つ44Dカテゴリ
 end  
 function c210000003.tgtg(e,c)
 	return e:GetHandler():GetLinkedGroup():IsContains(c)
